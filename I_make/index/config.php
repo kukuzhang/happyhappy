cc2b12b441c26a76e009437ec3c2b965ae8fa38a
mxlenet <mxle@mxle.net>
2014-03-20 12:03:21 +0800

    del

--- I_make/index/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kukuzhang
- * Date: 14-3-20
- * Time: 上午10:03
- */ 
--- I_public/I_/I_.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kukuzhang
- * Date: 14-3-20
- * Time: 上午10:10
- */
-include('../../i_use/i_core/i_.php');
